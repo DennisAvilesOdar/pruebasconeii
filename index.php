d6774acb5c78b3d1c76355b1f395e811dacd9d94
Dennis Mauricio Aviles Odar <dennisaviles@MacBook-Pro-de-Dennis.local>
2017-01-30 15:13:40 -0500

    .

---
+++ index.php
@@ -0,0 +1,2 @@
+<?php
+  header("location:uploads/index.php");
---
+++ uploads/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>nuevo</title>
+  </head>
+  <body>
+    <form enctype="multipart/form-data" action="uploader.php" method="post">
+      <input type="file" name="uploadedfile">
+      <input type="submit" value="Subir archivo">
+    </form>
+  </body>
+</html>
